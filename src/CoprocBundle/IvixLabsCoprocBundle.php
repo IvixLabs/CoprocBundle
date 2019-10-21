36597a1f81a4de0a00abd14f48e96fbdb6fa3b8c
Ivan Ruder <ivixlabs@gmail.com>
2019-10-21 12:11:28 +0700

    Add slave aware interface for tag services.

--- src/CoprocBundle/IvixLabsCoprocBundle.php
+++ src/CoprocBundle/IvixLabsCoprocBundle.php
@@ -2,6 +2,7 @@
 namespace IvixLabs\CoprocBundle;
 
 use IvixLabs\CoprocBundle\DependencyInjection\CoprocSlaveCompilerPass;
+use IvixLabs\CoprocBundle\Manager\SlaveAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -10,6 +11,9 @@ class IvixLabsCoprocBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+        $container
+            ->registerForAutoconfiguration(SlaveAwareInterface::class)
+            ->addTag('ivixlabs.coproc.slave');
         $container->addCompilerPass(new CoprocSlaveCompilerPass());
     }
 
